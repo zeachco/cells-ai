aa161b70eccb98d717fb1994142202ce77691e96
Olivier Rousseau-Guyot <olivier.rousseau.guyot@stay22.com>
2025-11-20 21:50:18 -0500

    ci: rust build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import path from "path";
+import fs from "fs";
 
 // Custom plugin to watch WASM file and trigger full reload
 function wasmReloadPlugin() {
@@ -21,12 +22,50 @@ function wasmReloadPlugin() {
           });
         }
       });
+
+      // Serve WASM file at /cells.wasm during dev
+      server.middlewares.use((req, res, next) => {
+        if (req.url === "/cells.wasm") {
+          if (fs.existsSync(wasmPath)) {
+            res.setHeader("Content-Type", "application/wasm");
+            fs.createReadStream(wasmPath).pipe(res);
+          } else {
+            res.statusCode = 404;
+            res.end("WASM file not found");
+          }
+        } else {
+          next();
+        }
+      });
+    },
+  };
+}
+
+// Custom plugin to copy WASM file to dist during build
+function wasmCopyPlugin() {
+  return {
+    name: "wasm-copy",
+    closeBundle() {
+      const srcPath = path.resolve(
+        "target/wasm32-unknown-unknown/release/cells.wasm",
+      );
+      const destPath = path.resolve("dist/cells.wasm");
+
+      if (fs.existsSync(srcPath)) {
+        fs.copyFileSync(srcPath, destPath);
+        console.log("WASM file copied to dist");
+      } else {
+        console.warn("WASM file not found at:", srcPath);
+      }
     },
   };
 }
 
 export default defineConfig({
-  plugins: [wasmReloadPlugin()],
+  plugins: [wasmReloadPlugin(), wasmCopyPlugin()],
+  base: process.env.GITHUB_REPOSITORY
+    ? `/${process.env.GITHUB_REPOSITORY.split("/")[1]}/`
+    : "/",
   server: {
     port: 3000,
   },
